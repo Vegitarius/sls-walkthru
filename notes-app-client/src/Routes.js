cd16843336d63a13e16ee04ea148e44268b45ebd
Vegitarius <gordonv15@gmail.com>
2018-12-10 21:15:10 -0800

    added signup comp and route

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
+import Signup from './containers/Signup';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 import AppliedRoute from './components/AppliedRoute';
@@ -11,6 +12,7 @@ export default ({ childProps }) =>
   <Switch>
     {/* needs exact otherwise matches any path that starts with / */}
     <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path='/signup' exact component={Signup} prop={childProps} />
     <AppliedRoute path='/login' exact component={Login} props={childProps} />
 
     {/* used to catch and direct 404 errors properly */}
