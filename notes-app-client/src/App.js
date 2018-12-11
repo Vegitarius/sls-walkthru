c783b0ed055d2bcf8590a2934d36e997b4538544
Vegitarius <gordonv15@gmail.com>
2018-12-10 20:05:19 -0800

    login functional, logout button, added routes

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -6,7 +6,28 @@ import './App.css';
 import Routes from './Routes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App container">
       {/* Bootstrap navbar, fits to container width with fluid */}
@@ -21,16 +42,22 @@ class App extends Component {
           <Navbar.Collapse>
             <Nav pullRight>
             {/* uses react bootstrap router to route page without refreshing */}
-              <LinkContainer to='/signup'>
-                <NavItem href='/signup'>Signup</NavItem>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <NavItem href='/login'>Login</NavItem>
-              </LinkContainer>
+            {/* conditional render whether user has been auth'd or not */}
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : <Fragment>
+                    <LinkContainer to='/signup'>
+                      <NavItem>SignUp</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+              }
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -3,14 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
 import Login from './containers/Login';
-import  NotFound from './containers/NotFound';
+import NotFound from './containers/NotFound';
+import AppliedRoute from './components/AppliedRoute';
 
-export default () => 
+export default ({ childProps }) => 
   // switch component renders first matching route
   <Switch>
     {/* needs exact otherwise matches any path that starts with / */}
-    <Route path="/" exact component={Home} />
-    <Route path='/login' exact component={Login} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path='/login' exact component={Login} props={childProps} />
 
     {/* used to catch and direct 404 errors properly */}
     <Route component={NotFound} />
