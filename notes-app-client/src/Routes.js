38f53e8ea432edace70dea9606f890b0594d6399
Vegitarius <gordonv15@gmail.com>
2018-12-11 17:11:27 -0800

    added note function to display each note when clicked

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -6,6 +6,7 @@ import Signup from './containers/Signup';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
 import AppliedRoute from './components/AppliedRoute';
 
 export default ({ childProps }) => 
@@ -16,7 +17,8 @@ export default ({ childProps }) =>
     <AppliedRoute path='/signup' exact component={Signup} prop={childProps} />
     <AppliedRoute path='/login' exact component={Login} props={childProps} />
     <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps} />
-    
+    <AppliedRoute path='/notes/:id' exact component={Notes} props={childProps} />
+
 
     {/* used to catch and direct 404 errors properly */}
     <Route component={NotFound} />
